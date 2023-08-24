a4357510fffd4c898a414bc00443fc16e9ea9cbe
Mikhail Suslov <suslov.mikhail@vector-tech.io>
2023-08-24 16:23:05 +0800

    Refactor for launching in Tomcat

---
+++ src/main/java/com/suslov/spring/LibraryAccounting.java
@@ -0,0 +1,12 @@
+package com.suslov.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAccounting {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAccounting.class, args);
+    }
+}
---
+++ src/test/java/com/suslov/spring/LibraryAccountingTests.java
@@ -0,0 +1,13 @@
+package com.suslov.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAccountingTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
